20a54e6d145eaab818f5f1dcb6d80ad17b61aad0
Magistry <Magistrigusti@yandex.ru>
2024-01-12 19:39:10 +0200

    0.01

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,18 +1,22 @@
 import styles from './Main.module.css';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton';
 import { useEffect, useState } from 'react';
 import { getNews } from '../../api/apiNews';
 
 const Main = () => {
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true);
         const response = await getNews();
         setNews(response.news);
-        console.log(news);
+        console.log(response);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -23,11 +27,18 @@ const Main = () => {
 
   return (
     <main className={styles.main}>
-      {
-        news.length > 0 ? <NewsBanner item={news[19]} /> : null
+      {news.length > 0 && !isLoading ? (
+        <NewsBanner item={news[19]} />
+      ) : (
+        <Skeleton type={'banner'} count={1} />
+      )
       }
 
-      <NewsList news={news} />
+      {!isLoading ? (
+        <NewsList news={news} />
+      ) : (
+        <Skeleton type={'banner'} count={10} />
+      )}
     </main>
   )
 };
